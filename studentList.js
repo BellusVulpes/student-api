1dfb545579644d0b2b9a1959e294dffe7f0a08ca
Daniel Amoateng <danny@Daniels-MacBook-Pro.local>
2021-05-12 20:51:00 -0400

    Adds new student

--- studentList.js
+++ studentList.js
@@ -4,6 +4,12 @@ const studentList = [
     lastName: "Jamison",
     age: 31,
     favoritePart: "React"
+  },
+  {
+    firstName: "Daniel",
+    lastName: "Amoateng",
+    age: 34,
+    favoritePart: "Html"
   }
 ]
 
